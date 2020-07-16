94d346da081fb407e4f34aa47c7414e98156155b
Abhishek Kumar <abhishek10001kumar@gmail.com>
2020-07-16 12:04:18 +0530

    fix:removed plugin from config

--- .github/workflows/release.config.js
+++ .github/workflows/release.config.js
@@ -1,13 +1,5 @@
 module.exports = {
     debug: true,
-    plugins: [
-        // analyze commits with conventional-changelog
-        ['@semantic-release/commit-analyzer'],
-        // generate changelog content with conventional-changelog
-        ['@semantic-release/release-notes-generator'],
-        // creating a new version commit
-        '@semantic-release/github',
-    ],
     branches: [
         "master",
         "next",
